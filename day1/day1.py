b371b414b8736631362e35237f1a6f73e8fdb1a8
Kevin Nastase <kevin.nastase@gmail.com>
2022-12-03 00:54:57 -0500

    clean up comments

--- day1/day1.py
+++ day1/day1.py
@@ -4,7 +4,5 @@ elves = [e.split('\n')for e in elves ]
 calorie_list = []
 for elf in elves:
     calorie_list.append(sum([int(e) for e in elf if e]))
-    # if calories > most:
-    #     most = calories
 
 print(sum(sorted(calorie_list)[-3:]))
